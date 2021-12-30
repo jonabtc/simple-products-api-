254d4638c817b0fc4886516fd6d167fe5a144213
jonabtc <oclesjo@gmail.com>
2021-12-30 13:01:37 -0500

    Manage authorization on products

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as productsController from '../controllers/products.controller';
+import { verifyToken } from "../middlewares";
 
 const router = Router();
 
-router.delete('/:productId', productsController.deleteProductById)
+router.delete('/:productId', verifyToken, productsController.deleteProductById)
 router.get('/:productId', productsController.getProductById)
 router.get('/', productsController.getProducts)
-router.post('/', productsController.createProduct)
-router.put('/:productId', productsController.updateProductById)
+router.post('/', verifyToken, productsController.createProduct)
+router.put('/:productId', verifyToken, productsController.updateProductById)
 
 export default router;
