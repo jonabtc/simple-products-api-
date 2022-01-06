2f668b411a64b93f600aa697489205d19aa2fe72
jonabtc <oclesjo@gmail.com>
2022-01-06 10:33:22 -0500

    Use dotenv for read environment variables

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://nodeuser:<password>@first-cluster.1i7v5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@first-cluster.1i7v5.mongodb.net/products-api-bd?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(db => console.log('DB is connected'))
-    .catch(error => console.log)
+    .catch(console.error)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app'
 import './database'
-const PORT = 8081;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('Server listen on port', PORT);
